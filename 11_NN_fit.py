b3771bd043e115e70ee81a0482adf7e4acc6ae86
shryu8902 <shryu8902@naver.com>
2020-05-12 21:06:15 +0900

    0512

--- 11_NN_fit.py
+++ 11_NN_fit.py
@@ -12,13 +12,19 @@ import tensorflow as tf
 from tensorflow.keras import layers, Sequential
 from tensorflow.keras import Input
 from tensorflow.keras.models import Model
-from tensorflow.keras.layers import LSTM, TimeDistributed, Dense, Activation, BatchNormalization, InputLayer, Lambda, Concatenate, concatenate,Reshape
+from tensorflow.keras.layers import LSTM, TimeDistributed, Dense, Activation, BatchNormalization, InputLayer, Lambda, Concatenate, concatenate,Reshape, RepeatVector
 from tensorflow.keras.callbacks import ModelCheckpoint, ReduceLROnPlateau, EarlyStopping
 from tensorflow.keras import optimizers
 from tensorflow.keras import backend as K
+from sklearn.metrics import r2_score
 
+#%%
 os.chdir(os.path.dirname(os.path.realpath(__file__)))
-
+def normal_augmentation(input, output, seed, std=0.2, times=5):
+    aug_input = np.vstack([(input*std*np.random.normal(size=input.shape)+input) for i in range(times)])
+    aug_input = np.vstack([input, aug_input])
+    aug_output = np.vstack([output for i in range(times+1)])
+    return(aug_input, aug_output)
 #%% 미국 주별 인구데이터 불러오기
 import pickle
 with open('US_pop.pkl', 'rb') as f:
@@ -69,17 +75,22 @@ for STATE in state_list:
         X_lnC.append(df[['lnC']].iloc[-61:-1].values)
         Y_dlnC.append(df[['dlnC']].iloc[-61:-1].values)
         Y_lnC.append(df[['lnC_nxt']].iloc[-61:-1].values)
+
 X_lnC = np.array(X_lnC)
 X = np.array(X)
 Y_dlnC = np.array(Y_dlnC)
 Y_lnC = np.array(Y_lnC)
+#%% Augmentation
+X_aug, Y_dlnC_aug = normal_augmentation(X,Y_dlnC,seed = 1, times = 20)
+X_lnC_aug, Y_lnC_aug = normal_augmentation(X_lnC, Y_lnC, seed = 1, times = 20)
 
 # %%
+
 # optimizer='RMSprop', init='glorot_uniform', lr=0.001, dropout=0.2, epoch = 10, time_len = 10,n_hidden=10,n_layer =1,loss='mean_squared_error'
 def CALC_dlnC(x):
     alpha = x[:,0]
-    A = x[:,1]
-    rho = x[:,2]
+    rho = x[:,1]
+    A = x[:,2]
     B = x[:,3]
     dlnT = x[:,4]
     CT = x[:,5]
@@ -89,21 +100,41 @@ def CALC_dlnC(x):
 main_input = Input(shape = (60,2),name = 'main')
 lnC_input = Input(shape = (60,1), name ='lnC')
 
-params = LSTM(50,dropout=0.2,kernel_initializer = 'glorot_uniform', return_sequences = True)(main_input)
-params = TimeDistributed(Dense(4))(params)
-merge = concatenate([params, main_input],axis=-1)
+# params = LSTM(50,dropout=0.2,kernel_initializer = 'glorot_uniform', return_sequences = True)(main_input)
+# params = TimeDistributed(Dense(4))(params)
+
+params = LSTM(10,dropout=0.2,kernel_initializer = 'glorot_uniform')(main_input)
+params2 = Dense(4, activation = 'tanh')(params)
+params3 = RepeatVector(60)(params2)
+
+merge = concatenate([params3, main_input],axis=-1)
 dlnC_hat = TimeDistributed(Lambda(CALC_dlnC))(merge)
 dlnC_hat2 = Reshape((60,1))(dlnC_hat)
 
-model = Model(inputs = [main_input,lnC_input], outputs = [dlnC_hat2, params])
+model = Model(inputs = [main_input,lnC_input], outputs = [dlnC_hat2, params3])
 
-model.compile(loss='mean_squared_error',optimizer='RMSprop',loss_weights=[1., 0.0])
+model.compile(loss='mean_squared_error',optimizer='Adam',loss_weights=[1., 0.0])
 #%%
 d= np.repeat(Y_lnC, 4, axis=2) ## dummy
 
-history = model.fit([X,X_lnC], [Y_dlnC,d], epochs = 2000, batch_size = 5, shuffle = True,verbose = 1)
+history = model.fit([X,X_lnC], [Y_dlnC,d], epochs = 1000, batch_size = 5, shuffle = True,verbose = 1)
+history = model.fit([X[0].reshape(1,60,2),X_lnC[0].reshape(1,60,1)], [Y_dlnC[0].reshape(1,60,1),d[0].reshape(1,60,4)], epochs = 1000, batch_size = 1, shuffle = True,verbose = 1)
 
 #%%
+Y_C_hat, values = model.predict([X[50].reshape(1,60,2),X_lnC[50].reshape(1,60,1)])
+
+Y_dlnC_hat, params_hat = model.predict([X, X_lnC])
+
+r2_score(Y_dlnC[0,:,:].reshape(-1), Y_dlnC_hat[1,:,:].reshape(-1))
+for i in range(1):
+    print(r2_score(Y_dlnC[i,:,:].reshape(-1), Y_dlnC_hat[i,:,:].reshape(-1))
+)
+
+
+model.predict()
+
+
+
 #%%
 fig, loss_ax = plt.subplots()
 acc_ax = loss_ax.twinx()
@@ -111,8 +142,11 @@ acc_ax = loss_ax.twinx()
 loss_ax.plot(history.history['loss'], 'y', label='train loss')
 loss_ax.set_xlabel('epoch')
 loss_ax.set_ylabel('loss')
+
 loss_ax.legend(loc='upper left')
 plt.show()
+
+
 
 #%%
 tf.keras.utils.plot_model(model)
